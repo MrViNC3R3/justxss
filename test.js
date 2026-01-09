e65df0910f734959f9f00814ac0c0f5d18a6bbf9
MrViNC3R3 <rogervincere2@gmail.com>
2026-01-09 13:30:23 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
-<h1>test</h1>
-console.log("Hello")
+console.log("[CSP TEST] jsDelivr external script executed successfully");
+
+document.documentElement.setAttribute(
+  "data-csp-jsdelivr-test",
+  "executed"
+);
